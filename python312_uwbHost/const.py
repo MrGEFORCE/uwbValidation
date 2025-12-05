07025c1cc36510abad11b4945da192efe9d79d7c
WaveGod <1296961194@qq.com>
2025-12-05 14:54:29 +0800

    新增UDP配置参数

--- python312_uwbHost/const.py
+++ python312_uwbHost/const.py
@@ -3,6 +3,12 @@ import p312.pConfig as pConfig
 FREQS = []
 ETHERNET_BUFFER_LEN = 1024
 
-cfg = pConfig.CONFIG()
+# 默认 UDP 配置，可根据实际设备地址修改
+UDP_LOCAL_HOST = "192.168.240.2"
+UDP_LOCAL_PORT = 2
+UDP_REMOTE_HOST = "192.168.240.1"
+UDP_REMOTE_PORT = 1
+UDP_SOCKET_TIMEOUT = 1.0  # seconds
 
+cfg = pConfig.CONFIG()
 cfg.DARK_THEME = True
